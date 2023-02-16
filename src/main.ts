ddd6671cb3c1b49c5d168ea291f31c82d996da7f
sagarjariwala333 <sagarjariwala333@gmail.com>
2023-02-16 21:17:22 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   //const app = await NestFactory.create(AppModule,{ cors: true });
   const app = await NestFactory.create(AppModule);
+  /*app.enableCors({
+  	origin:[
+  		'https://mylibray-view.vercel.app/'
+  	],
+  	methods: ['GET','POST'],
+  	credentials: true
+  });*/
   await app.listen(3000);
 }
 bootstrap();
